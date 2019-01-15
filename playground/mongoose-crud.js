d37bba0b91d9a36a4d69f5f344e79f16d860e5ae
Omar Khairy <omarkhairy21@gmail.com>
2019-01-15 02:38:05 +0200

    Add DELETE /todos/:id Route

--- playground/mongoose-crud.js
+++ playground/mongoose-crud.js
@@ -1,2 +1,11 @@
 const {mongoose} = require('./../server/db/mongoose');
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
+
+const PersonName = new Schema({
+    name:String,
+    age:Number,
+    isSingle:Boolean,
+    birthday:Date,
+    description:Buffer
+})
+
--- server/server.js
+++ server/server.js
@@ -36,6 +36,7 @@ app.get('/todos',(req,res)=>{
         res.status(400).send(e);
     })
 })
+
 app.get('/todos/:id',(req,res)=>{
     var id = req.params.id;
 
@@ -48,10 +49,28 @@ app.get('/todos/:id',(req,res)=>{
         }
 
         res.status(200).send(todo);
-    }),(e)=>{
+    }).catch((e)=>{
         res.status(400).send(e);
-    }
+    });
 });
+app.delete('/todos/:id', (req,res)=>{
+    // get the id 
+    var id = req.params.id;
+        if(!ObjectID.isValid(id)){
+          return res.status(400).send()
+        }
+    Todo.findByIdAndRemove(id).then((todo)=>{
+        if(!todo) {
+            return res.status(404).send()
+        }
+
+        res.send(todo);
+    }).catch((e)=>{
+        res.status(400).send(e);
+    })    
+    //validate
+
+})
 app.listen(3000, () => {
     console.log('Started on port 3000')
 });
